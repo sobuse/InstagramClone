28a15703b4e2b30146038f0e4231e59343cd51f4
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-09 16:25:04 +0100

    Update Post.cs

--- src/InstagramClone.Api/Entities/Post.cs
+++ src/InstagramClone.Api/Entities/Post.cs
@@ -3,7 +3,7 @@
     public class Post
     {
         
-        public Guid PostId { get; set; }
+        public Guid Id { get; set; }
         public string Content { get; set; }
         public DateTime CreatedDate { get; set; }
         public User User { get; set; }
