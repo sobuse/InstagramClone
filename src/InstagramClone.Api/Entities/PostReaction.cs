e0d5f71988af6d4bb1eca653974094bc183f1bd8
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-09 19:13:11 +0200

    Removed wrong property and changed property name

--- src/InstagramClone.Api/Entities/PostReaction.cs
+++ src/InstagramClone.Api/Entities/PostReaction.cs
@@ -5,9 +5,8 @@
      
         public Post Post{ get; set; }
         public Guid Id { get; set; }
-        public User User { get; set; }
         public User Author { get; set; }
-        public int ReactionType { get; set; }
+        public int Type { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
