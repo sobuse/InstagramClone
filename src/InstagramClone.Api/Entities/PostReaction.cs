7c2c0855b9e279ff4a4ebda8100607ec69ffc4af
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-13 00:35:36 +0100

    changed user to Author

--- src/InstagramClone.Api/Entities/PostReaction.cs
+++ src/InstagramClone.Api/Entities/PostReaction.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public Post Post{ get; set; } //Navigation property
         public Post PostId { get; set; } //FK
-        public User User { get; set; } //Navigation property
+        public User Author { get; set; } //Navigation property
         public User AuthorId { get; set; } //FK
         public int Type { get; set; }
         public DateTime CreatedDate { get; set; }
