d68257cad4a0c4d17be765ecf4dbea9f41960787
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-14 00:22:26 +0100

    configured one to many relationship

--- src/InstagramClone.Api/Database/InstagramCloneDbContext.cs
+++ src/InstagramClone.Api/Database/InstagramCloneDbContext.cs
@@ -10,18 +10,28 @@ namespace InstagramClone.Api.Database
 
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<PostReaction> PostReactions { get; set; }
-        public DbSet<UserFollower> UserFollowers { get; set; }
+       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasKey(u => u.Id);
             modelBuilder.Entity<Post>().HasKey(p => p.Id);
             modelBuilder.Entity<PostReaction>().HasKey(pr => pr.Id);
+            modelBuilder.Entity<UserFollower>().HasKey(userFollower => userFollower.FollowedUserId);
+
+
+
+            modelBuilder.Entity<Post>().HasOne<User>(post => post.Author).WithMany( user => user.Posts);
+           
+
+           
         }
        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReaction> PostReactions { get; set; }
+        public DbSet<UserFollower> UserFollowers { get; set; }
+
 
     }
 }
