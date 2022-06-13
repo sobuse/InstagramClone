69e0162a61ee0ead326ceb9477cd13b3b61284f5
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-13 20:38:11 +0100

    Created my dbContext class and add DbSet properties

---
+++ src/InstagramClone.Api/Database/InstagramCloneDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InstagramClone.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Api.Database
+{
+    public class InstagramCloneDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReaction> PostReactions { get; set; }
+        public DbSet<UserFollower> UserFollowers { get; set; }
+
+    }
+}
