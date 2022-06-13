086eea226ff8ac5f50744e220103ff3aa49e5d5d
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-13 21:53:34 +0100

    used fluent api to configure my primary keys

--- src/InstagramClone.Api/Database/InstagramCloneDbContext.cs
+++ src/InstagramClone.Api/Database/InstagramCloneDbContext.cs
@@ -5,10 +5,23 @@ namespace InstagramClone.Api.Database
 {
     public class InstagramCloneDbContext : DbContext
     {
+        public InstagramCloneDbContext(): base()
+        {
+
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<PostReaction> PostReactions { get; set; }
         public DbSet<UserFollower> UserFollowers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<Post>().HasKey(p => p.Id);
+            modelBuilder.Entity<PostReaction>().HasKey(pr => pr.Id);
+        }
+       
 
     }
 }
