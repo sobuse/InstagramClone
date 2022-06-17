30d0218f7d3c255e972f2f483588f052c8d15534
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-17 15:35:41 +0100

    configured Dbcontext in Program.cs

--- src/InstagramClone.Api/Program.cs
+++ src/InstagramClone.Api/Program.cs
@@ -1,5 +1,13 @@
+using InstagramClone.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<InstagramCloneDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnectionString"));
+});
 
 app.MapGet("/", () => "Hello World!");
 
