96505a4565f80458e64b29695767d99f4cc53539
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-19 00:21:30 +0100

    updated my guid

--- src/InstagramClone.Api/Configuration/UserConfiguration.cs
+++ src/InstagramClone.Api/Configuration/UserConfiguration.cs
@@ -12,7 +12,7 @@ namespace InstagramClone.Api.Configuration
                 .HasData(
                     new User
                     {
-                        Id = Guid.NewGuid(),
+                        Id = new  Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21"),// 1
                         Email = "ehuerikenbaba@gmail.com",
                         Password = "12345",
                         FirstName = "Ehueriken",
@@ -20,14 +20,29 @@ namespace InstagramClone.Api.Configuration
                         Avatar = "Sammy.jpeg",
                         Posts = new List<Post> { new Post
                         {
-                            new Post(){Id = Guid.NewGuid(), Content = "Hello world" ,a }
-                        } }
+                       
+                                Id = Guid.NewGuid(), 
+                                Content = "Hello world" ,
+                                AuthorId = new  Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21"),
+                                PostReactions = new  List<PostReaction>{ new PostReaction()
+                                    { Id = Guid.NewGuid(), 
+                                        CreatedDate = DateTime.Now,
+                                        AuthorId =  new Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21"),
+                                        Type = 3,
+                                        PostId =  new  Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21") 
+                                    } 
+                                },
+                                CreatedDate =  DateTime.Now
+                            }
+                        } 
+                   
+
 
 
                     },
                     new User
                     {
-                        Id = Guid.NewGuid(),
+                        Id = new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E"), //2
                         Email = "ehueriken@gmail.com",
                         Password = "123456",
                         FirstName = "Samuel",
@@ -37,7 +52,7 @@ namespace InstagramClone.Api.Configuration
                     },
                     new User
                     {
-                        Id = Guid.NewGuid(),
+                        Id = new Guid("4FF2CD54-D7AC-4066-91D4-F646B0C5A6EA"),
                         Email = "baba@gmail.com",
                         Password = "1234567",
                         FirstName = "Ehimah",
