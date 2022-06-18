9d00b95fa0c0ef95137d7920a713523fea6738b8
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-19 00:22:03 +0100

    changed class name

--- src/InstagramClone.Api/Configuration/UserFollowerConfiguration.cs
+++ src/InstagramClone.Api/Configuration/UserFollowerConfiguration.cs
@@ -1,6 +1,27 @@
-﻿namespace InstagramClone.Api.Configuration
+﻿using InstagramClone.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Api.Configuration
 {
-    public class UserFollower
+    public  static class UserFollowerConfiguration
     {
+        public static void UserFollowerSeed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserFollower>().HasData(new UserFollower
+            {
+                FollowedUserId =   new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E"),
+                FollowerId = new Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21")
+            }, 
+            
+            new UserFollower
+            {
+                FollowerId = new Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21"),
+                FollowedUserId = new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E")
+            }
+
+                );
+
+            
+        }
     }
 }
