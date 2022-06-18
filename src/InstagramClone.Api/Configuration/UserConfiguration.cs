80765ad595972eb3b4689f9bed89740b05f1830e
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-19 00:17:57 +0100

    Seeded data

---
+++ src/InstagramClone.Api/Configuration/UserConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using InstagramClone.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Api.Configuration
+{
+    public static class UserConfiguration
+    {
+
+        public static void SeedUser(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Email = "ehuerikenbaba@gmail.com",
+                        Password = "12345",
+                        FirstName = "Ehueriken",
+                        LastName = "Obuse",
+                        Avatar = "Sammy.jpeg",
+                        Posts = new List<Post> { new Post
+                        {
+                            new Post(){Id = Guid.NewGuid(), Content = "Hello world" ,a }
+                        } }
+
+
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Email = "ehueriken@gmail.com",
+                        Password = "123456",
+                        FirstName = "Samuel",
+                        LastName = "Ehueriken",
+                        Avatar = "Sammy.jpeg"
+
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Email = "baba@gmail.com",
+                        Password = "1234567",
+                        FirstName = "Ehimah",
+                        LastName = "Obuse",
+                        Avatar = "Sammy.jpeg"
+
+                    }
+                ) ;
+        }
+    }
+}
---
+++ src/InstagramClone.Api/Configuration/UserFollowerConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace InstagramClone.Api.Configuration
+{
+    public class UserFollower
+    {
+    }
+}
