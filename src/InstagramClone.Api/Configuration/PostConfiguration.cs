a7a3ead52d11893f3fe3a5a0991c59e84cfc2858
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-20 01:42:20 +0200

    created additional static class to seed post and postReaction

---
+++ src/InstagramClone.Api/Configuration/PostConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using InstagramClone.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Api.Configuration
+{
+    public static class PostConfiguration
+    {
+        public static void SeedPost(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(new Post
+            {
+                Id = new Guid("B09F27AC-8CAE-4C87-9E7F-4A1CBB7925F2"),
+                Content = "My instagram clone",
+                AuthorId = new Guid(),
+                CreatedDate = DateTime.Now,
+               
+            },
+            new Post
+            {
+                Id = new Guid("9E09FC2D-3A7D-4AC0-B58B-83D256A3FA95"),
+                Content = "My second post",
+                AuthorId = new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E"),   // 2
+                CreatedDate = DateTime.Now,
+                
+            }
+            ); ;
+        }
+    }
+}
---
+++ src/InstagramClone.Api/Configuration/PostReactionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using InstagramClone.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Api.Configuration
+{
+    public static  class PostReactionConfiguration
+    {
+        public static void SeedPostReaction(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PostReaction>().HasData(
+                new PostReaction
+                {
+                    Id = new Guid("3EAEFC7B-AEB4-422B-B517-95B1DACC4CE6"),
+                    PostId = new Guid("9E09FC2D-3A7D-4AC0-B58B-83D256A3FA95"), //2 from post
+                    AuthorId = new Guid("9E09FC2D-3A7D-4AC0-B58B-83D256A3FA95"),
+                    Type = 2,
+                    CreatedDate = DateTime.Now
+                }
+                );
+        }
+    }
+}
