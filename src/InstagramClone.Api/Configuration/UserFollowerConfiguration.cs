6e86784ac751ac35e9f2fcd2ab6b94351e5ad30c
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-21 11:23:57 +0100

    cleaned up unnecessary comment and white space

--- src/InstagramClone.Api/Configuration/UserFollowerConfiguration.cs
+++ src/InstagramClone.Api/Configuration/UserFollowerConfiguration.cs
@@ -12,17 +12,8 @@ namespace InstagramClone.Api.Configuration
                 FollowedUserId =   new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E"), // user 2
                 FollowerId = new Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21"), // user 1
                 CreatedDate = DateTime.Now
-            }
-            //new UserFollower
-            //{
-                
-            //    FollowedUserId = new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E"),
-            //    FollowerId = new Guid("4FF2CD54-D7AC-4066-91D4-F646B0C5A6EA") 
-            //}
-            
-                );
-
-            
+            });
+   
         }
     }
 }
