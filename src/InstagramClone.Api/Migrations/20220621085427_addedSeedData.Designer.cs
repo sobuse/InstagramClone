20103d4ab95268371326b5c0ebb51d5c0ad7efaf
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-21 10:42:40 +0100

    Ran migration
    * Edited migration folder manually to make proper changes to new fill created
    
    * Ran migration

--- src/InstagramClone.Api/Migrations/20220621085427_addedSeedData.Designer.cs
+++ src/InstagramClone.Api/Migrations/20220621085427_addedSeedData.Designer.cs
@@ -4,6 +4,7 @@ using InstagramClone.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InstagramClone.Api.Migrations
 {
     [DbContext(typeof(InstagramCloneDbContext))]
-    partial class InstagramCloneDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220621085427_addedSeedData")]
+    partial class addedSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/InstagramClone.Api/Migrations/20220621085427_addedSeedData.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstagramClone.Api.Migrations
+{
+    public partial class addedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PostReactions",
+                keyColumn: "Id",
+                keyValue: new Guid("3eaefc7b-aeb4-422b-b517-95b1dacc4ce6"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 54, 26, 483, DateTimeKind.Local).AddTicks(1772));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: new Guid("9e09fc2d-3a7d-4ac0-b58b-83d256a3fa95"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 54, 26, 483, DateTimeKind.Local).AddTicks(1734));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: new Guid("b09f27ac-8cae-4c87-9e7f-4a1cbb7925f2"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 54, 26, 483, DateTimeKind.Local).AddTicks(1729));
+
+            migrationBuilder.UpdateData(
+                table: "UserFollowers",
+                keyColumns: new[] { "FollowedUserId", "FollowerId" },
+                keyValues: new object[] { new Guid("88e18c3a-8ec4-4008-b36e-f1925525472e"), new Guid("50ed31b9-30ae-4e7a-beef-5997eaba8a21") },
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 54, 26, 483, DateTimeKind.Local).AddTicks(1752));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("50ed31b9-30ae-4e7a-beef-5997eaba8a21"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 54, 26, 483, DateTimeKind.Local).AddTicks(1522));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PostReactions",
+                keyColumn: "Id",
+                keyValue: new Guid("3eaefc7b-aeb4-422b-b517-95b1dacc4ce6"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 48, 11, 938, DateTimeKind.Local).AddTicks(1867));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: new Guid("9e09fc2d-3a7d-4ac0-b58b-83d256a3fa95"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 48, 11, 938, DateTimeKind.Local).AddTicks(1823));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: new Guid("b09f27ac-8cae-4c87-9e7f-4a1cbb7925f2"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 48, 11, 938, DateTimeKind.Local).AddTicks(1816));
+
+            migrationBuilder.UpdateData(
+                table: "UserFollowers",
+                keyColumns: new[] { "FollowedUserId", "FollowerId" },
+                keyValues: new object[] { new Guid("88e18c3a-8ec4-4008-b36e-f1925525472e"), new Guid("50ed31b9-30ae-4e7a-beef-5997eaba8a21") },
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 48, 11, 938, DateTimeKind.Local).AddTicks(1848));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("50ed31b9-30ae-4e7a-beef-5997eaba8a21"),
+                column: "CreatedDate",
+                value: new DateTime(2022, 6, 21, 9, 48, 11, 938, DateTimeKind.Local).AddTicks(1518));
+        }
+    }
+}
