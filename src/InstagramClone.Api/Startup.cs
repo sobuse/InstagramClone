010400ede5687cd99a91b3fc86708c354a8fb24c
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-06-24 22:11:05 +0100

    reconfigured startUp class

--- src/InstagramClone.Api/Startup.cs
+++ src/InstagramClone.Api/Startup.cs
@@ -5,6 +5,7 @@ namespace InstagramClone.Api
 {
     public static class Startup
     {
+
         public static WebApplication InitialIzeApp(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -13,26 +14,32 @@ namespace InstagramClone.Api
             Configure(app);
             return app;
         }
-       
+
 
         private static void ConfigureService(WebApplicationBuilder builder)
         {
-            
+            //builder.Services.AddApplicationService();
+            builder.Services.AddControllers();
+            builder.Services.AddCors();
+
             builder.Services.AddDbContext<InstagramCloneDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 
-               
+
             });
-          
+
         }
 
         private static void Configure(WebApplication app)
         {
-           
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
             app.MapGet("/", () => "Hello World!");
-            
-            
+
+
         }
     }
 }
