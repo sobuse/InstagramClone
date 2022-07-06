d6faf0060c09b4894b3444fb3b00845e76571a7b
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-07-06 01:32:48 +0100

    Fixed routing

--- src/InstagramClone.Api/Controllers/UsersController.cs
+++ src/InstagramClone.Api/Controllers/UsersController.cs
@@ -2,17 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Http;
+
 using InstagramClone.Api.Database;
 using InstagramClone.Api.Entities;
-
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace InstagramClone.Api.Controllers
 {
 
-    [RoutePrefix("api/users")]
-    public class UsersController : ApiController
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
     {
         private readonly InstagramCloneDbContext _context;
         public UsersController(InstagramCloneDbContext context)
@@ -20,14 +21,14 @@ namespace InstagramClone.Api.Controllers
             _context = context;
         }
         [HttpGet]
-        [Route("{id:Guid}")]
-        public IHttpActionResult GetUser(Guid id)
+        [Route("{id}")]
+        public IActionResult GetUser(Guid id)
         {
             // user variable is used to store the value of a particuler user from the database
             // This finds a user by id from the db set user property from the context class
             var user = _context.Users.Find(id);
             if (user == null)
-            {   
+            {
                 // this give a 404 error response when the user they searched for by id could not be found 
                 return NotFound();
             }
