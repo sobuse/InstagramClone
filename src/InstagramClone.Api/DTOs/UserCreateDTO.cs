16c1c92e58f657d2422b6bd8048f2bd493030bea
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-07-10 14:17:24 +0100

    added a new property to the class

--- src/InstagramClone.Api/DTOs/UserCreateDTO.cs
+++ src/InstagramClone.Api/DTOs/UserCreateDTO.cs
@@ -8,5 +8,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Avatar { get; set; }
+        public DateTime CreatedDate { get; set; }   
     }
 }
