2eb386db231b134f1a0860f709d47325562736f0
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-07-12 23:45:34 +0100

    created followersDto class and properties

---
+++ src/InstagramClone.Api/DTOs/FollowersDto.cs
@@ -0,0 +1,9 @@
+﻿namespace InstagramClone.Api.DTOs
+{
+    public class FollowersDto
+    {
+        public Guid FollowedUserId { get; set; }
+        public Guid FollowerId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
