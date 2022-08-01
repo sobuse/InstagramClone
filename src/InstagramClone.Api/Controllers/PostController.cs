77617f0ed0b8512481c0b4e3bd69f3b0086aae6d
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-08-01 20:54:36 +0100

    Created createpost controller

--- src/InstagramClone.Api/Controllers/PostController.cs
+++ src/InstagramClone.Api/Controllers/PostController.cs
@@ -1,4 +1,6 @@
 ﻿using InstagramClone.Api.Database;
+using InstagramClone.Api.DTOs;
+using InstagramClone.Api.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InstagramClone.Api.Controllers
@@ -24,6 +26,39 @@ namespace InstagramClone.Api.Controllers
                 return BadRequest();
             }
             return Ok(post);
+        }
+
+        [HttpPost("")]
+        public IActionResult CreatePost([FromBody]PostCreateDto postCreateDto)
+        {
+            if (postCreateDto == null)
+            {
+                return BadRequest();
+            }
+
+            Post InsertPost = new Post()
+            {
+                Id = postCreateDto.Id,
+                Content = postCreateDto.Content,
+                AuthorId = postCreateDto.AuthorId,
+                CreatedDate = postCreateDto.CreatedDate,
+            };
+            var postIdExist = context.Posts.Any(p=>p.Id == postCreateDto.Id);
+            var authorIdExist = context.Posts.Any(a=>a.AuthorId == postCreateDto.AuthorId);
+            if (postIdExist)
+            {
+                return Conflict();
+            }
+            if (authorIdExist == null)
+            {
+                return NotFound();
+            }
+            this.context.Posts.Add(InsertPost);// [Guid("4794A969-7DE6-4E00-8FBE-D515BE13B596")]
+            context.SaveChanges();
+
+            // if there is post id return conflct
+            // author id must exixt in the database
+            return Ok(InsertPost);
         }
        
     }
