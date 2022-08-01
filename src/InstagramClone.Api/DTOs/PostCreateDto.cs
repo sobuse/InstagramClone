e7ab2f4cc9d7a5e05184d6231053a1aa77fd83d5
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-08-01 20:55:07 +0100

    created postdto class

---
+++ src/InstagramClone.Api/DTOs/PostCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace InstagramClone.Api.DTOs
+{
+    public class PostCreateDto
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
