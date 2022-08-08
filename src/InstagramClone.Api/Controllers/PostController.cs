ed07ea2a359f34816a2c315cc21ccce19c8e85c0
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2022-08-08 11:42:46 +0100

    IC-14: Create Post API (#12)
    * Created createpost controller
    
    * created postdto class
    
    * made changes to controller method
    
    * Changed variable name
    * made correction to validation check

--- src/InstagramClone.Api/Controllers/PostController.cs
+++ src/InstagramClone.Api/Controllers/PostController.cs
@@ -1,4 +1,6 @@
 ﻿using InstagramClone.Api.Database;
+using InstagramClone.Api.DTOs;
+using InstagramClone.Api.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InstagramClone.Api.Controllers
@@ -24,6 +26,43 @@ namespace InstagramClone.Api.Controllers
                 return BadRequest();
             }
             return Ok(post);
+        }
+
+        [HttpPost("")]
+        public IActionResult CreatePost([FromBody]PostCreateDto postCreateDto)
+        {
+            if (postCreateDto == null)
+            {
+                return BadRequest();
+            }
+
+            Post postToInsert = new Post()
+            {
+                Id = postCreateDto.Id,
+                Content = postCreateDto.Content,
+                AuthorId = postCreateDto.AuthorId,
+                CreatedDate = postCreateDto.CreatedDate,
+            };
+            var postIdExist = context.Posts.Any(p=>p.Id == postCreateDto.Id);
+            var authorIdExist = context.Posts.Any(a=>a.AuthorId == postCreateDto.AuthorId);
+
+            // if there is post id return conflct
+            if (postIdExist)
+            {
+                return Conflict();
+            }
+
+            // author id must exit in the database
+            if (authorIdExist == false)
+            {
+                return NotFound();
+            }
+            this.context.Posts.Add(postToInsert);
+            context.SaveChanges();
+            
+            
+           
+            return Ok(postToInsert);
         }
        
     }
