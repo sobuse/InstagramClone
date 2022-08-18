0c80152f193b5cc2b642b412c6d59c34b662b048
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-08-18 09:02:08 +0100

    Changed to Identity Password

--- src/InstagramClone.Api/Configuration/UserConfiguration.cs
+++ src/InstagramClone.Api/Configuration/UserConfiguration.cs
@@ -16,7 +16,7 @@ namespace InstagramClone.Api.Configuration
                     {
                         Id = new  Guid("50ED31B9-30AE-4E7A-BEEF-5997EABA8A21"),//  user 1
                         Email = "ehuerikenbaba@gmail.com",
-                        Password = "12345",
+                        PasswordHash = hasher.HashPassword(null, "12345"),
                         FirstName = "Ehueriken",
                         LastName = "Obuse",
                         Avatar = "Sammy.jpeg",
@@ -28,7 +28,7 @@ namespace InstagramClone.Api.Configuration
                     {
                         Id = new Guid("88E18C3A-8EC4-4008-B36E-F1925525472E"), // user 2
                         Email = "ehueriken@gmail.com",
-                        Password = "123456",
+                        PasswordHash = hasher.HashPassword(null, "12345"),
                         FirstName = "Samuel",
                         LastName = "Ehueriken",
                         Avatar = "Sammy.jpeg"
@@ -38,7 +38,7 @@ namespace InstagramClone.Api.Configuration
                     {
                         Id = new Guid("4FF2CD54-D7AC-4066-91D4-F646B0C5A6EA"),  // user 3
                         Email = "baba@gmail.com",
-                        Password = "1234567",
+                        PasswordHash = hasher.HashPassword(null, "12345"),
                         FirstName = "Ehimah",
                         LastName = "Obuse",
                         Avatar = "Sammy.jpeg"
