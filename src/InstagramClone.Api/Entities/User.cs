9d0088d6c031ba58f8aa507e85e6d113c24efa82
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-08-18 09:06:53 +0100

    Update User.cs

--- src/InstagramClone.Api/Entities/User.cs
+++ src/InstagramClone.Api/Entities/User.cs
@@ -5,8 +5,8 @@ namespace InstagramClone.Api.Entities
     public class User: IdentityUser<Guid>
     {
         public Guid Id { get; set; } //PK
-        public string Email { get; set; }
-        public string Password { get; set; }
+       // public string Email { get; set; }
+        //public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Avatar { get; set; }
