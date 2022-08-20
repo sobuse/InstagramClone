d5e01640dc473fb1946e73ece753594972757514
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-08-20 09:02:21 +0100

    added extention class for roles configuration

---
+++ src/InstagramClone.Api/Configuration/ApplicationRoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using InstagramClone.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Api.Configuration
+{
+    public static class ApplicationRoleConfiguration
+    {
+        public static void SeedRole(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationRole>().HasData(new List< ApplicationRole>
+            {
+                new ApplicationRole
+                {
+                    Id = new Guid("A26DFA63-7B56-49AB-9452-9EFC992132F0"),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                              
+                }
+                
+            });
+        }
+    }
+}
