d291f42e9f2822fd4e7816e0bca6525ba6cf0a87
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-09-03 00:10:10 +0100

    Update UserCreateDTO.cs

--- src/InstagramClone.Api/DTOs/UserCreateDTO.cs
+++ src/InstagramClone.Api/DTOs/UserCreateDTO.cs
@@ -8,11 +8,11 @@ namespace InstagramClone.Api
         public Guid Id { get; set; } //PK
         public string Email { get; set; }
         [Required (ErrorMessage ="Password is required.")]
-        public string Password { get; set; }
+        public string Password { get; set; } 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Avatar { get; set; }
-        public ICollection<ApplicationRole> Roles { get; set; }
+        public ICollection<string> Roles { get; set; }
         public DateTime CreatedDate { get; set; }
         
     }
