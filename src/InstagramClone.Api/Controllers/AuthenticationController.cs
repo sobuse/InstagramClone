bf2c38e437b498a19f8988403d6eb06666797400
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-09-07 11:32:25 +0100

    Create AuthenticationController.cs

---
+++ src/InstagramClone.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using InstagramClone.Api.Authentication;
+using InstagramClone.Api.DTOs;
+using InstagramClone.Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace InstagramClone.Api.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly UserManager<User> userManager;
+        private readonly AuthenticationManager authenticationManager;
+
+        public AuthenticationController(UserManager<User> userManager, AuthenticationManager authenticationManager)
+        {
+            this.userManager = userManager;
+            this.authenticationManager = authenticationManager;
+        }
+
+       // [HttpPost("login")]
+        [Authorize]
+        public async Task<IActionResult> Authenticate([System.Web.Http.FromBody] UserForAuthenticationDto user)
+        {
+            if (!await authenticationManager.ValidateUser(user))
+            {
+                return Unauthorized();
+            }
+            return Ok(new { Token = await authenticationManager.CreateToken() });
+        }
+    }
+}
