77b856484bcf53689fc60f3925e86eb94edd0ec6
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-09-24 20:23:32 +0100

    reconfigured code base

--- src/InstagramClone.Api/Configuration/ConfigurationOfJwt.cs
+++ src/InstagramClone.Api/Configuration/ConfigurationOfJwt.cs
@@ -8,15 +8,18 @@ namespace InstagramClone.Api.Configuration
     {
         public static void JwtConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-            var jwtSettings = configuration.GetSection("jwtSettings");
+            var jwtSettings = configuration.GetSection("JwtSettings");
             var secretKey = Environment.GetEnvironmentVariable("SECRET");
+            //var secretKey = "SECRET";
 
             services.AddAuthentication(opt =>
             {
                 opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(opt =>
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+             .AddJwtBearer(options =>
             {
-                opt.TokenValidationParameters = new TokenValidationParameters
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
                     ValidateAudience = true,
