bae9e5074c07dc538580c908ffa1883685197b2b
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2022-10-05 10:36:29 +0100

    moved app.css and app.tsx to a folder called main

--- src/InstagramClone.FrontEnd/src/index.tsx
+++ src/InstagramClone.FrontEnd/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+window.React = React
 import './index.css';
-import App from './App';
+import App from './main/App';
 import reportWebVitals from './reportWebVitals';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+  
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,6 +14,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/InstagramClone.FrontEnd/src/main/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header subtitle='Instagram Clone'/>
+    </div>
+  )
+}
+export default App;
